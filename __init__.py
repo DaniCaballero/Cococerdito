652a82f345bac460eae37253cd31501912052bbf
Daniela Caballero <danielacaballero12@gmail.com>
2021-01-05 16:00:34 +0000

    changes made from theia lab enviroment

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SECRET_KEY='mikey',
+        SECRET_KEY='test',
         DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
         DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
         DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
